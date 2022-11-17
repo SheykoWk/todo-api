abed19807531800541f73781cb36b385dd4386e1
Sh3yk0  (Sahid Kick) <sheyko.wk@gmail.com>
2022-11-16 21:49:00 -0600

    start with refactor

---
+++ tasks/tasks.services.js
@@ -0,0 +1,9 @@
+//? En este archivo vamos a utilizar todo lo relacionado al REQ y RES
+
+const taskControllers = require('./tasks.controllers')
+
+//* taskControllers.createTodo()
+//* taskControllers.findAllTodos()
+//* taskControllers.findTodoById()
+
+
